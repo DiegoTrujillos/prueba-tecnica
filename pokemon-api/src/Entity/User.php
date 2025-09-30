832ede0c27b935e8a1653b6b7543837149ecf12d
Deigo <202100184@upqroo.edu.mx>
2025-09-30 17:28:28 -0500

    feat(auth): implementar autenticación con JWT usando json_login y User entity

--- pokemon-api/src/Entity/User.php
+++ pokemon-api/src/Entity/User.php
@@ -6,17 +6,19 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255, unique: true) ]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
@@ -25,9 +27,6 @@ class User
     #[ORM\Column]
     private array $roles = [];
 
-    /**
-     * @var Collection<int, Pokemon>
-     */
     #[ORM\OneToMany(targetEntity: Pokemon::class, mappedBy: 'trainer')]
     private Collection $pokemons;
 
@@ -44,7 +43,6 @@ class User
     public function setId(int $id): static
     {
         $this->id = $id;
-
         return $this;
     }
 
@@ -56,8 +54,12 @@ class User
     public function setUsername(string $username): static
     {
         $this->username = $username;
-
         return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
     }
 
     public function getPassword(): ?string
@@ -68,20 +70,24 @@ class User
     public function setPassword(string $password): static
     {
         $this->password = $password;
-
         return $this;
     }
 
     public function getRoles(): array
     {
-        return $this->roles;
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
-
         return $this;
+    }
+
+    public function eraseCredentials(): void
+    {
     }
 
     /**
@@ -98,19 +104,16 @@ class User
             $this->pokemons->add($pokemon);
             $pokemon->setTrainer($this);
         }
-
         return $this;
     }
 
     public function removePokemon(Pokemon $pokemon): static
     {
         if ($this->pokemons->removeElement($pokemon)) {
-            // set the owning side to null (unless already changed)
             if ($pokemon->getTrainer() === $this) {
                 $pokemon->setTrainer(null);
             }
         }
-
         return $this;
     }
 }
